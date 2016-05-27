6dd71d6c0f2f0f8938e08e91ac2e11ba097b82f8
Shane Canon <scanon@lbl.gov>
2016-05-27 14:37:34 -0700

    hack for deploy.cfg

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 . /kb/deployment/user-env.sh
 
-python ./scripts/prepare_deploy_cfg.py ./deploy.cfg ./work/config.properties
+#python ./scripts/prepare_deploy_cfg.py ./deploy.cfg ./work/config.properties
 
 if [ -f ./work/token ] ; then
   export KB_AUTH_TOKEN=$(<./work/token)
